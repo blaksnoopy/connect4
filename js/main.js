2dd6f1e01cc92edf71f82d95f484f6642da31349
Marquiese Solomon <blaksnoopy@Marquieses-MacBook-Pro.local>
2019-11-06 10:04:12 -0800

    select columns

--- js/main.js
+++ js/main.js
@@ -1,23 +1,40 @@
 //variables
-let player, board, winner, turn, colors, spaces
+let player, board, winner, playerTurn, colors, spaces,
+    container, column1, column2, column3, column4, column5, column6, column7;
 
 
 spaces = document.querySelectorAll('div');
-let container = document.querySelector('.container');
+container = document.querySelector('.container');
 colors = [
     emptySpace = null,
-    player1 = 'purple',
+    player1 = 'red',
     player2 = 'yellow'
 ]
+board = [
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+];
+
+column1 = document.querySelectorAll('.column1');
+column2 = document.querySelectorAll('.column2');
+column3 = document.querySelectorAll('.column3');
+column4 = document.querySelectorAll('.column4');
+column5 = document.querySelectorAll('.column5');
+column6 = document.querySelectorAll('.column6');
+column7 = document.querySelectorAll('.column7');
 
 
 
 //initialize game
 init();
 function init() {
-    turn = 1;
+    playerTurn = player1;
     winner = null;
-    container.addEventListener('click', handleClick);
 }
 
 
@@ -30,7 +47,6 @@ container.addEventListener('click', handleClick);
 
 function handleClick(evt) {
     if(evt.target.tagname !== 'DIV') return;
-    console.log(evt.target);
 }
 
 
